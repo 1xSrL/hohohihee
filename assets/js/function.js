63cfe0f23fed4926b042e40aa9798f720e067031
themightybananas <pancahisam18@gmail.com>
2023-11-17 19:36:07 +0700

    UPDATE | like counter

--- assets/js/function.js
+++ assets/js/function.js
@@ -14,3 +14,19 @@ if (timeDiffInHours < 24) {
     postTimeElement.textContent = "Hari ini";
 } else {
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const likeButtons = document.querySelectorAll(".like-button");
+    likeButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const likeCountId = button.getAttribute("data-likecount");
+            handleLikeButtonClick(likeCountId);
+        });
+    });
+});
+
+function handleLikeButtonClick(likeCountId) {
+    const likeCountElement = document.getElementById(likeCountId);
+    let currentLikeCount = parseInt(likeCountElement.textContent);
+    currentLikeCount++;
+    likeCountElement.textContent = currentLikeCount;
+}
