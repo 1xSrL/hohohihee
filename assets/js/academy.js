120d2e0fbcf1c23556aacf76f1f1c6297c4e031e
Muhammad Faruq <susilofaruq@gmail.com>
2023-11-21 11:20:57 +0700

    UPDATE | background color

--- assets/js/academy.js
+++
@@ -1,8 +0,0 @@
-function toggleLike() {
-    var likeButton = document.getElementById('likeButton');
-    if (likeButton.innerHTML === 'Like') {
-        likeButton.innerHTML = 'Dislike';
-    } else {
-        likeButton.innerHTML = 'Like';
-    }
-}
