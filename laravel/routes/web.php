f9a8353015ba553968e8557943f53682ef51e629
Nikhil Tegar <nikiltegarx@gmail.com>
2023-12-05 01:40:11 +0700

    UPDATE | Controller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,28 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
+ // 127.0.0.1:8000 ==> view welcome
+ Route::get('/', function () {
+    return view('welcome');
+ });
 
-Route::get('/siswa', function () {
-    return view('Welcome');
-});
+// // 127.0.0.1:8000/siswa ==> <h1>SAYA SIAPA</h1>
+// Route::get('/siswa', function () {
+//    return "<h1>SAYA SISWA</h1>";
+// });
+
+// // 127.0.0.1:8000/siswa/1 ==> <h1>SAYA SIAPA ID 1</h1>
+//Route::get('/siswa/{id}', function ($id) {
+//    return "<h1>SAYA SISWA ID  $id</h1>";
+// })->where('id', '[0-9]+');
+
+// 127.0.0.1:8000/siswa/1 ==> <h1>SAYA SIAPA ID 1</h1>
+// Route::get('/siswa/{id}/{nama}', function ($id,$nama) {
+//    return "<h1>SAYA SIAPA ID  $id & $nama </h1>";
+// })->where(['id', '[0-9]+', 'nama' => '[A-Za-z]+']);
+
+route::get('siswa',SiswaController::class,'index');
