b60bd97405ef6401145d380bf2397fc8f4522f07
Nikhil Tegar <nikiltegarx@gmail.com>
2023-12-06 22:52:46 +0700

    ADD | Blade template

--- laravel/app/Http/Controllers/SiswaController.php
+++ laravel/app/Http/Controllers/SiswaController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
     //
+    function index()
+    {
+        return'<h1>Saya SISWA dari Controller</h1>';
+    }
+     function detail($id)
+     {
+        return "<h1>Saya SISWA dari Controller dengan id $id</h1>"
+        ;
+     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\HalamanController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,24 +13,28 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
- // 127.0.0.1:8000 ==> view welcome
- Route::get('/', function () {
-    return view('welcome');
- });
-
-// // 127.0.0.1:8000/siswa ==> <h1>SAYA SIAPA</h1>
-// Route::get('/siswa', function () {
-//    return "<h1>SAYA SISWA</h1>";
+// 127.0.0.1:8000/ ==> view welcome
+// Route::get('/', function () {
+//    return view('welcome');
 // });
 
-// // 127.0.0.1:8000/siswa/1 ==> <h1>SAYA SIAPA ID 1</h1>
-//Route::get('/siswa/{id}', function ($id) {
-//    return "<h1>SAYA SISWA ID  $id</h1>";
-// })->where('id', '[0-9]+');
+// // 127.0.0.1:8000/siswa ==> <h1>SAYA JERAPAH</h1>
+// Route::get('/siswa', function () {
+//     return "<h1>SAYA JERAPAH</h1>";
+// });
 
-// 127.0.0.1:8000/siswa/1 ==> <h1>SAYA SIAPA ID 1</h1>
-// Route::get('/siswa/{id}/{nama}', function ($id,$nama) {
-//    return "<h1>SAYA SIAPA ID  $id & $nama </h1>";
-// })->where(['id', '[0-9]+', 'nama' => '[A-Za-z]+']);
+//
+// // 127.0.0.1:8000/siswa/1 ==> <h1>SAYA JERAPAH DENGAN ID 1</h1>
+// Route::get('/siswa/{id}', function ($id) {
+//     return "<h1>SAYA JERAPAH DENGAN ID $id</h1>";
+// })->where('id','[0-9]+');
 
-route::get('siswa',SiswaController::class,'index');
+//
+// Route::get('/siswa/{id}/{nama}', function ($id, $nama) {
+//     return "<h1>SAYA JERAPAH DENGAN ID $id & NAMA $nama</h1>";
+// })->where(['id'=>'[0-9]+','nama'=>'[A-Za-z]+']);
+
+Route::get('siswa',[SiswaController::class,'index']);
+Route::get('siswa/{id}',[SiswaController::class,'detail'])->where('id', '[0-9]+');
+
+Route::get('siswa',[HalamanController::class,'index']);
